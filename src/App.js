266b089e0b81fbc1c730498701e156824ad90401
Dwi Darma <dwikbtn15@gmail.com>
2019-12-09 13:49:00 +0800

    bug

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import {Robots} from './Robots';
 import SearchBox from './SearchBox';
-const App = () => {
-	return (
-		<div className='tc'>
-		<h1>RoboFriends</h1>
-		<SearchBox/>
-		<CardList Robots={Robots}/>
-		</div>
-	);
+
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			Robots: Robots,
+			searchField: ''	
+		}
+	}
+
+	onSearchChange = (event) => {
+		const filteredRobots = this.state.Robots.filter(Robot => {
+			return Robots.name.toLowerCase().includes(this.searchField.toLowerCase());
+			})
+		console.log(filteredRobots);
+		}
+	render(){
+		return (
+			<div className='tc'>
+			<h1>RoboFriends</h1>
+			<SearchBox searchChange={this.onSearchChange}/>
+			<CardList Robots={this.state.Robots}/>
+			</div>
+	);	
+	}
 }
+
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({searchChange}) => {
 	return (
 		<div className='pa2'> {/*pa 2= padding 2 perasal dari tachon*/} 
 			<input
 			className='pa3 ba b--green bg-lightest-blue'
 			type="search" 
-			placeholder='Search robots'/>
+			placeholder='Search robots'
+			onChange={searchChange}
+			/>
 		</div>
 	);
 }
