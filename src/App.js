8749f5672464739ec48c0f6b1e828d891d51e1db
Dwi Darma <dwikbtn15@gmail.com>
2019-12-10 09:17:43 +0800

    add styles

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import {Robots} from './Robots';
+import { Robots } from './Robots';
 import SearchBox from './SearchBox';
-
+import './App.css';
 
 class App extends Component {
 	constructor() {
-		super()
+		super();
 		this.state = {
 			Robots: Robots,
-			searchField: ''	
-		}
+			searchField: ''
+		};
 	}
 
-	onSearchChange = (event) => {
-		const filteredRobots = this.state.Robots.filter(Robot => {
-			return Robots.name.toLowerCase().includes(this.searchField.toLowerCase());
-			})
-		console.log(filteredRobots);
-		}
-	render(){
+	onSearchChange = event => {
+		this.setState({ searchField: event.target.value });
+	};
+
+	render() {
+		let robots = this.state.Robots;
+		// let searchField = this.state.searchField;
+
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+		});
+
 		return (
 			<div className='tc'>
-			<h1>RoboFriends</h1>
-			<SearchBox searchChange={this.onSearchChange}/>
-			<CardList Robots={this.state.Robots}/>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<CardList Robots={filteredRobots} />
 			</div>
-	);	
+		);
 	}
 }
-
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const SearchBox = ({searchChange}) => {
+const SearchBox = ({ searhfield, searchChange}) => {
 	return (
-		<div className='pa2'> {/*pa 2= padding 2 perasal dari tachon*/} 
+		<div className='pa2'>
 			<input
 			className='pa3 ba b--green bg-lightest-blue'
 			type="search" 
 			placeholder='Search robots'
 			onChange={searchChange}
 			/>
-		</div>
+		</div>  
 	);
 }
 
